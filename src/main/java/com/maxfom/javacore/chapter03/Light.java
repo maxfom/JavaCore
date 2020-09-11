ce65385811231caae82e682a32e4366b27f0de0b
Max <max.fomiche@gmail.com>
2020-09-11 12:10:53 +0300

    Example 5 - BlockTest (Block Code)

---
+++ src/main/java/com/maxfom/javacore/chapter03/Light.java
@@ -0,0 +1,26 @@
+package main.java.com.maxfom.javacore.chapter03;
+/*
+    Вычисляем расстояние, проходимое светом.
+    Использует переменные типа long.
+ */
+class Light {
+    public static void main(String[] args) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        // приблизительная скорость света, миль в секунду
+        lightspeed = 186000;
+
+        days = 1000; // указать количество дней
+
+        seconds = days * 24 * 60 * 60; // преобразуем в секунды
+
+        distance = lightspeed * seconds;
+
+        System.out.print("За " + days);
+        System.out.print(" дней свет пройдет около ");
+        System.out.print(distance + " миль");
+    }
+}
