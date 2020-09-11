2cd8d2fa2c9c928102c6838608d9279f9679cd73
Max <max.fomiche@gmail.com>
2020-09-11 11:17:24 +0300

    Example 4 - ForTest (loop "for")

---
+++ src/main/java/com/maxfom/javacore/chapter02/ForTest.java
@@ -0,0 +1,14 @@
+package main.java.com.maxfom.javacore.chapter02;
+/*
+    Продемонстрировать применение цикла for.
+
+    Присвоить исходному файлу имя "ForTest.java"
+ */
+class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; x++)
+            System.out.println("Значение x: " + x);
+    }
+}
