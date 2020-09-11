ac38dec408c7a0082488ed9dfdaf12ff29f9b6ee
Max <max.fomiche@gmail.com>
2020-09-11 13:34:57 +0300

    Example 9 - Type "char" look like "int"

---
+++ src/main/java/com/maxfom/javacore/chapter03/CharDemo2.java
@@ -0,0 +1,15 @@
+package main.java.com.maxfom.javacore.chapter03;
+/*
+    Символьные переменные ведут себя как целочисленные значения
+ */
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 содержит " + ch1);
+
+        ch1++;
+        System.out.println("ch1 теперь содержит " + ch1);
+    }
+}
