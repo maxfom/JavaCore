17ac4f081cdc09e6b82269fb13ac50aed6753f58
Max <max.fomiche@gmail.com>
2020-09-11 10:15:29 +0300

    Example 1

---
+++ src/main/java/com/maxfom/javacore/chapter02/Example.java
@@ -0,0 +1,12 @@
+package main.java.com.maxfom.javacore.chapter02;
+/*
+    Это простая программа на Java.
+    Присвоить исходному файлу имя "Example.java"
+ */
+
+class Example {
+    // Эта программа начинается с вызова метода main()
+    public static void main(String[] args) {
+        System.out.println("Простая программа На Java.");
+    }
+}
