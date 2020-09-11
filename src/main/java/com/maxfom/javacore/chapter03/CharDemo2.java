c6ea1b6769066eeb49fca6a29817409d9bafd5cf
Max <max.fomiche@gmail.com>
2020-09-11 13:46:57 +0300

    Example 9 - Type "char" look like "int"

--- src/main/java/com/maxfom/javacore/chapter03/CharDemo2.java
+++ src/main/java/com/maxfom/javacore/chapter03/CharDemo2.java
@@ -5,7 +5,6 @@ package main.java.com.maxfom.javacore.chapter03;
 public class CharDemo2 {
     public static void main(String[] args) {
         char ch1;
-
         ch1 = 'X';
         System.out.println("ch1 содержит " + ch1);
 
