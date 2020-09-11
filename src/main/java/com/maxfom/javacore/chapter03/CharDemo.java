f09238fca02132920cefefac440fa5a866d691fe
Max <max.fomiche@gmail.com>
2020-09-11 13:30:49 +0300

    Example 8 - Type "char"

---
+++ src/main/java/com/maxfom/javacore/chapter03/CharDemo.java
@@ -0,0 +1,15 @@
+package main.java.com.maxfom.javacore.chapter03;
+/*
+    Продемонстрировать применение типа данных char
+ */
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88; // код символа 'X'
+        ch2 = 'Y';
+
+        System.out.print("ch1 и ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
