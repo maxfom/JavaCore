3f69292fa04871eec99ca2177295680aa6049304
Max <max.fomiche@gmail.com>
2020-09-12 22:32:28 +0300

    Example 11 - Dinamic initialisation

---
+++ src/main/java/com/maxfom/javacore/chapter03/DynInit.java
@@ -0,0 +1,13 @@
+package main.java.com.maxfom.javacore.chapter03;
+/*
+    В этом примере демонстрируется динамическая инициализация
+ */
+public class DynInit {
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+
+        // динамическая инициализация переменной c
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("Гипотенуза равна " + c);
+    }
+}
