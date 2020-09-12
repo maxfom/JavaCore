6e56ac0830b2949b8d8968a66d689b038eb5230f
Max <max.fomiche@gmail.com>
2020-09-12 22:47:46 +0300

    Example 13 - Lifetime of variable

---
+++ src/main/java/com/maxfom/javacore/chapter03/LifeTime.java
@@ -0,0 +1,18 @@
+package main.java.com.maxfom.javacore.chapter03;
+/*
+    Продемонстрировать срок действия переменной
+ */
+public class LifeTime {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 3; x++) {
+            int y = -1; // переменная y инициализуется при
+                        // каждом вхождении в блок кода
+            System.out.println("y равно: " + y);    // здесь всегда
+                                                    // выводится значение -1
+            y = 100;
+            System.out.println("y теперь равно: " + y);
+        }
+    }
+}
