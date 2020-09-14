ccf4e469db9c1d9bca72493eead50d37007cb70a
Max <max.fomiche@gmail.com>
2020-09-14 12:01:36 +0300

    Example 17 - New clever AutoArray

---
+++ src/main/java/com/maxfom/javacore/chapter03/AutoArray.java
@@ -0,0 +1,10 @@
+package main.java.com.maxfom.javacore.chapter03;
+/*
+    Усовершенствованная версия предыдущей программы с массивами
+ */
+public class AutoArray {
+    public static void main(String[] args) {
+        int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        System.out.println("В апреле " + month_days[3] + " дней.");
+    }
+}
