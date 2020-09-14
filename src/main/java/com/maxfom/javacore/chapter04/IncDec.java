96d5d0d04c4489e1c537999ea1ac71914ead5c2d
Max <max.fomiche@gmail.com>
2020-09-14 17:30:21 +0300

    Example 26 - Increment operation

---
+++ src/main/java/com/maxfom/javacore/chapter04/IncDec.java
@@ -0,0 +1,17 @@
+package main.java.com.maxfom.javacore.chapter04;
+// Применение операции инкремента ++
+public class IncDec {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c;
+        int d;
+        c = ++b;
+        d = a++;
+        c++;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+    }
+}
