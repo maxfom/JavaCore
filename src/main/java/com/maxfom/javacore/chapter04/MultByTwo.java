e3ba3c7c3ec005afd5b759c719f55f75bfd779ee
Max <max.fomiche@gmail.com>
2020-09-15 12:30:49 +0300

    Example 29 - MultByTwo exercise

---
+++ src/main/java/com/maxfom/javacore/chapter04/MultByTwo.java
@@ -0,0 +1,13 @@
+package main.java.com.maxfom.javacore.chapter04;
+// Применение сдвига влево в качестве быстрого способа умножения на 2
+public class MultByTwo {
+    public static void main(String[] args) {
+        int i;
+        int num = 0xFFFFFFE;
+
+        for (i = 0; i < 4; i++) {
+            num = num << 1;
+            System.out.println(num);
+        }
+    }
+}
