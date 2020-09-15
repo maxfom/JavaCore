e040527f8e5f39b0cce2e782c108146677d6a9e3
Max <max.fomiche@gmail.com>
2020-09-15 17:20:00 +0300

    Example 43 - Hard (!) build Menu for loops

---
+++ src/main/java/com/maxfom/javacore/chapter05/Menu.java
@@ -0,0 +1,53 @@
+package main.java.com.maxfom.javacore.chapter05;
+import java.util.Scanner;
+// Используем оператор цикла do-while для выбора пункта меню
+public class Menu {
+    public static void main(String[] args) {
+            char choice;
+            do {
+                System.out.println("Справка по оператору:");
+                System.out.println("1. if");
+                System.out.println("2. switch");
+                System.out.println("3. while");
+                System.out.println("4. do-while");
+                System.out.println("5. for\n");
+                Scanner z = new Scanner(System.in);
+                choice = z.next().charAt(0);
+            }   while (choice < '1' || choice > '5');
+
+            System.out.println("\n");
+
+            switch (choice) {
+                case '1':
+                    System.out.println("if:\n");
+                    System.out.println("if(условие) оператор;");
+                    System.out.println("else оператор;");
+                    break;
+                case '2':
+                    System.out.println("switch:\n");
+                    System.out.println("switch(выражение) {");
+                    System.out.println(" case константа:");
+                    System.out.println(" последовательность операторов");
+                    System.out.println(" break;");
+                    System.out.println(" // ...");
+                    System.out.println("}");
+                    break;
+                case '3':
+                    System.out.println("while:\n");
+                    System.out.println("while(условие) оператор;");
+                    break;
+                case '4':
+                    System.out.println("do-while:\n");
+                    System.out.println("do {");
+                    System.out.println(" оператор;");
+                    System.out.println("} while (условие)");
+                    break;
+                case '5':
+                    System.out.println("for:\n");
+                    System.out.println("for(инициализация; условие; итерация)");
+                    System.out.println(" оператор;");
+                    break;
+
+            }
+    }
+}
