8cee3c78c3ace1269d90f6314f365d4ba87d0f97
Max <max.fomiche@gmail.com>
2020-09-15 16:50:24 +0300

    Example 40 - While (Loop)

---
+++ src/main/java/com/maxfom/javacore/chapter05/While.java
@@ -0,0 +1,11 @@
+package main.java.com.maxfom.javacore.chapter05;
+// Применение оператора цикла while
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n > 0) {
+            System.out.println("Такт " + n);
+            n--;
+        }
+    }
+}
