90c20a62c59c3ac863081e87b262c556a31b1484
Max <max.fomiche@gmail.com>
2020-09-16 00:09:00 +0300

    Example 53 - Nested - loop for in loop for

---
+++ src/main/java/com/maxfom/javacore/chapter05/Nested.java
@@ -0,0 +1,13 @@
+package main.java.com.maxfom.javacore.chapter05;
+//  Циклы могут быть вложенными
+public class Nested {
+    public static void main(String[] args) {
+        int i, j;
+
+        for (i = 0; i < 10; i++)    {
+            for (j = i; j < 10; j++)
+                System.out.print(".");
+            System.out.println();
+        }
+    }
+}
