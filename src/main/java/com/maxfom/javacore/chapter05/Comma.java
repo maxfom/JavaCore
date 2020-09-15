209966ca851df856518f5ec963086bc43cab8ed5
Max <max.fomiche@gmail.com>
2020-09-15 23:31:18 +0300

    Example 46 - Comma with loop For

---
+++ src/main/java/com/maxfom/javacore/chapter05/Comma.java
@@ -0,0 +1,12 @@
+package main.java.com.maxfom.javacore.chapter05;
+//  Использование запятой в операторе цикла for
+public class Comma {
+    public static void main(String[] args) {
+        int a, b;
+
+        for (a = 1, b = 4; a < b; a++, b--) {
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+    }
+}
