0ec45216d1e1615d12cf90800c0ae5c5f1c680dd
Max <max.fomiche@gmail.com>
2020-09-16 00:32:57 +0300

    Example 61 - continue - with mark

---
+++ src/main/java/com/maxfom/javacore/chapter05/ContinueLabel.java
@@ -0,0 +1,16 @@
+package main.java.com.maxfom.javacore.chapter05;
+//  Применение continue с меткой
+public class ContinueLabel {
+    public static void main(String[] args) {
+        outer: for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (j > i) {
+                System.out.println();
+                continue outer;
+            }
+            System.out.print(" " + (i * j));
+        }
+        }
+        System.out.println();
+    }
+}
