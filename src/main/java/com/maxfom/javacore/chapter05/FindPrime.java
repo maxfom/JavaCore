43acb58fee32e13ab120a45358e0929585c45bff
Max <max.fomiche@gmail.com>
2020-09-15 23:29:06 +0300

    Example 45 - Example - find prime with loops

---
+++ src/main/java/com/maxfom/javacore/chapter05/FindPrime.java
@@ -0,0 +1,24 @@
+package main.java.com.maxfom.javacore.chapter05;
+/*
+    Проверка на простые числа
+ */
+public class FindPrime {
+    public static void main(String[] args) {
+        int num;
+        boolean isPrime;
+
+        num = 13;
+
+        if (num < 2) isPrime = false;
+        else isPrime = true;
+
+        for(int i = 2; i <= num/i; i++) {
+            if ((num % i) == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) System.out.println("Простое число");
+        else System.out.println("Не простое число");
+    }
+}
