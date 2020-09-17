4fbc82a117e2b425b263a46dec3176c8a1666932
Max <max.fomiche@gmail.com>
2020-09-17 21:25:35 +0300

    Example 76 - argument call on value

---
+++ src/main/java/com/maxfom/javacore/chapter07/CallByValue.java
@@ -0,0 +1,21 @@
+package main.java.com.maxfom.javacore.chapter07;
+// Аргументы примитивных типов передаются по значению
+class Tests {
+    void meth(int i, int j) {
+        i *= 2;
+        j /= 2;
+    }
+}
+public class CallByValue {
+    public static void main(String[] args) {
+        Tests ob = new Tests();
+
+        int a = 15, b = 20;
+
+        System.out.println("a и b до вызова: " + a + " " + b);
+
+        ob.meth(a , b);
+
+        System.out.println("a и b после вызова: " + a + " " + b);
+    }
+}
