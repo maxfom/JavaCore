933c968365360b3ef1c652a9a3968495a0af18d5
Max <max.fomiche@gmail.com>
2020-09-17 22:13:58 +0300

    Example 83 - try static

---
+++ src/main/java/com/maxfom/javacore/chapter07/UseStatic.java
@@ -0,0 +1,21 @@
+package main.java.com.maxfom.javacore.chapter07;
+// Демонстрация статических переменных, методов и блоков кода
+public class UseStatic {
+    static  int a = 3;
+    static int b;
+
+    static void meth(int x) {
+        System.out.println("x = " + x);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    static  {
+        System.out.println("Статический блок инициализирован.");
+        b = a * 4;
+    }
+
+    public static void main(String[] args) {
+        meth(42);
+    }
+}
