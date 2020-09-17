f16cc63b552e1856164108ab1515316c4138b886
Max <max.fomiche@gmail.com>
2020-09-17 23:00:52 +0300

    Example 91 - arguments on command string

---
+++ src/main/java/com/maxfom/javacore/chapter07/CommandLine.java
@@ -0,0 +1,8 @@
+package main.java.com.maxfom.javacore.chapter07;
+// Вывести все аргументы командной строки
+public class CommandLine {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+            System.out.println("args[" + i + "]: " + args[i]);
+    }
+}
