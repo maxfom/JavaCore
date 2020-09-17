06606eaa7922ad59ad3ef9dfce8fdcb1f1d65594
Max <max.fomiche@gmail.com>
2020-09-17 21:49:34 +0300

    Example 80 - recursion again

---
+++ src/main/java/com/maxfom/javacore/chapter07/Recursion2.java
@@ -0,0 +1,25 @@
+package main.java.com.maxfom.javacore.chapter07;
+// Другой пример рекурсии
+class RecTest {
+    int values[];
+    RecTest(int i) {
+        values = new int[i];
+    }
+
+    // вывести рекурсивно элементы массива
+    void printArray(int i) {
+        if (i == 0) return;
+        else printArray(i - 1);
+        System.out.println("[" + (i - 1) + "] " + values[i-1]);
+    }
+}
+public class Recursion2 {
+    public static void main(String[] args) {
+        RecTest ob = new RecTest(10);
+        int i;
+
+        for (i = 0; i < 10; i++) ob.values[i] = i;
+
+        ob.printArray(10);
+    }
+}
