1dde6ab31ca52f286080dd3d25ef79b039213d07
Max <max.fomiche@gmail.com>
2020-09-18 21:54:19 +0300

    Example 94 - array with variable arguments and simple arguments

---
+++ src/main/java/com/maxfom/javacore/chapter07/VarArgs2.java
@@ -0,0 +1,22 @@
+package main.java.com.maxfom.javacore.chapter07;
+//  Использовать аргументы переменной длины вместе
+// со стандартными аргументами
+public class VarArgs2 {
+
+    // в данном примере msg - обычный параметр,
+    // а v - параметр переменной длины
+    static void vaTest(String msg, int ... v)   {
+        System.out.print(msg + v.length + " Содержимое: ");
+
+        for (int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        vaTest("Один параметр переменной длины: ", 10);
+        vaTest("Три параметра переменной длины: ", 1, 2, 3 );
+        vaTest("Без параметров переменной длины: ");
+    }
+}
