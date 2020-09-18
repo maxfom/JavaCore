b471a3c279e1f4efc6acb5be946f0c0a4ba94e26
Max <max.fomiche@gmail.com>
2020-09-18 21:46:32 +0300

    Example 92 - array with variable arguments

---
+++ src/main/java/com/maxfom/javacore/chapter07/PassArray.java
@@ -0,0 +1,27 @@
+package main.java.com.maxfom.javacore.chapter07;
+/*
+    Использовать массив для переачи методу переменного
+    количества аргументов. Это старый подход к обработке
+    аргументов переменной длины
+ */
+class PassArray {
+    static void vaTest(int v[]) {
+        System.out.println("Количество аргументов: " + v.length + " Содержимое: ");
+
+        for (int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Обратите внимание на порядок создания массива
+        // для хранения аргументов.
+        int n1[] = { 10 };
+        int n2[] = { 1, 2, 3 };
+        int n3[] = { };
+        vaTest(n1); // 1 аргумент
+        vaTest(n2); // 3 аргумента
+        vaTest(n3); // без аргументов
+    }
+
+}
